58e6711bbf55d4d13974f9cd27c9ca0fdda7b720
unclay <wcl58981298@gmail.com>
2023-03-02 16:04:37 +0800

    build base

--- index.ts
+++ index.ts
@@ -1,6 +1,65 @@
 #!/usr/bin/env node
 
-import * as fs from 'node:fs'
-import * as path from 'node:path'
+import * as fs from "node:fs";
+import * as path from "node:path";
 
-console.log(12333)
+import minimist from "minimist";
+import prompts from "prompts";
+import banners from "./utils/banners";
+import renderTemplate from "./utils/render-template";
+
+let result = {
+  name: "react-project",
+};
+async function init() {
+  console.log();
+  console.log(
+    process.stdout.isTTY && process.stdout.getColorDepth() > 8
+      ? banners.gradientBanner
+      : banners.defaultBanner
+  );
+  console.log();
+
+  const cwd = process.cwd();
+  // possible options:
+  const argv = minimist(process.argv.slice(2), {
+    alias: {},
+    string: ["_"],
+    // all arguments are treated as booleans
+    boolean: true,
+  });
+  let targetDir = argv._[0];
+  const defaultProjectName = !targetDir ? "react-project" : targetDir;
+
+  result = await prompts([
+    {
+      type: targetDir ? null : "text",
+      name: "name",
+      message: "Project name",
+      initial: defaultProjectName,
+      onState: (state) =>
+        (targetDir = String(state.value).trim() || defaultProjectName),
+    },
+  ]);
+
+  const root = path.join(cwd, targetDir);
+
+  // todo:
+  // work around the esbuild issue that `import.meta.url` cannot be correctly transpiled
+  // when bundling for node and the format is cjs
+  // const templateRoot = new URL('./template', import.meta.url).pathname
+  const templateRoot = path.resolve(__dirname, "template");
+  const render = function render(templateName) {
+    const templateDir = path.resolve(templateRoot, templateName);
+    renderTemplate(templateDir, root);
+  };
+
+  // Render base template
+  render("base");
+}
+
+try {
+  init();
+} catch (err) {
+  console.error(err);
+}
